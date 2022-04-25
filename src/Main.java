082ba71529af1bc7118af11f1daa9e9fcc16ecbe
Денис <den_2005@list.ru>
2022-04-25 19:47:46 +0300

    HW1.4 complete

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Задача 1
+        System.out.println("Задание 1");
+        int i = 0;
+        while (i<10) {
+            i++;
+            System.out.print(i + " " );
+        }
+        System.out.println();
+        for (;i>0;i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задание 2");
+        int firstFr = 3;
+        for (i = firstFr; i<=31; i+=7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задание 3");
+        int yearCurrent = 2022, yearFirst = yearCurrent - 200, yearLast = yearCurrent + 100;
+        for (int year = yearFirst; year <= yearLast; year++) {
+            if (year % 79 == 0) {
+                System.out.println(year);
+            }
+        }
+
     }
 }
