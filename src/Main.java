40a0aeed238044e45caef702fc6ccbdf787357f6
Денис <den_2005@list.ru>
2022-04-30 22:22:56 +0300

    HW1.4 tasks4,5

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        //Задача 1
+/*        //Задача 1
         System.out.println("Задание 1");
         int i = 0;
         while (i<10) {
@@ -28,7 +28,7 @@ public class Main {
             }
         }
         System.out.println();
-
+*/
         System.out.println("Задание 4");
         for (int j = 1; j<31; j++) {
             if (j % 3 == 0 && j % 5 == 0) {
