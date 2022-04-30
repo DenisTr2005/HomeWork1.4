738818ae5459019ce0404bfffa2d5de7c8489908
Денис <den_2005@list.ru>
2022-04-30 22:13:41 +0300

    HW1.4 tasks4,5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
+        //Задача 1
+        System.out.println("Задание 1");
+        int i = 0;
+        while (i<10) {
+            i++;
+            System.out.print(i + " " );
+        }
+        System.out.println();
+        for (;i>0;i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задание 2");
+        int firstFr = 3;
+        for (i = firstFr; i<=31; i+=7) {
+            System.out.println("Сегодня пятница, " + i + "-е число. Необходимо подготовить отчет.");
+        }
+        System.out.println();
+
+        System.out.println("Задание 3");
+        int yearCurrent = 2022, yearFirst = 200, yearLast = 100;
+        for (i = 0; i <= yearCurrent + yearLast; i+=79) {
+            if (i >= yearCurrent - yearFirst) {
+                System.out.println(i);
+            }
+        }
+        System.out.println();
+
         System.out.println("Задание 4");
         for (int j = 1; j<31; j++) {
             if (j % 3 == 0 && j % 5 == 0) {
